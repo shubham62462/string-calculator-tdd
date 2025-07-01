371ce989ed4759257a6645705cdfd5d21c153edd
Shubham Dey <shubham@byldd.com>
2025-07-01 22:10:05 +0530

    Add: handle single number input

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -3,3 +3,7 @@ const { add } = require('../src/stringCalculator');
 test('returns 0 for empty string', () => {
     expect(add("")).toBe(0);
 });
+
+test('returns the number itself for single number', () => {
+    expect(add("1")).toBe(1);
+});
